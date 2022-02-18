544e5bd0e4df147ac2cc03914f736a821e81d366
drevil2 <98413084+drevil2@users.noreply.github.com>
2022-02-18 11:40:49 -0500

    396. Javascript Expressions in JSX & ES6 Template Literals

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fName = "Danny";
+const lName = "Franco";
+const num = 7;
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Pizza</li>
-      <li>Lasagna</li>
-      <li>Beef</li>
-    </ul>
+    <h1>
+      {fName} {lName}
+    </h1>
+    <h1>{fName + " " + lName}</h1>
+    <h1>{`${fName} ${lName}`}</h1>
+    <p>Your lucky number is {num}</p>
+    <p>Your lucky number is {3 + 4}</p>
+    <p>Your lucky number is {Math.random()}</p>
   </div>,
   document.getElementById("root")
 );
